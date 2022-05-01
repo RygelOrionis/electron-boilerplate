b31b04e778e94ba824eb060adfed37ff3407f4fd
mirco.centrone <mirco.centrone@nexusat.it>
2022-05-01 11:29:13 +0200

    Node Modules Updated

--- src/modules/x/app/app.js
+++ src/modules/x/app/app.js
@@ -1,9 +1,29 @@
 import { LightningElement } from 'lwc';
+import fetchDataHelper from './fetchDataHelper';
 
-export default class FooApp extends LightningElement {
+const columns = [
+    { label: 'Label', fieldName: 'name' },
+    { label: 'Website', fieldName: 'website', type: 'url' },
+    { label: 'Phone', fieldName: 'phone', type: 'phone' },
+    { label: 'Balance', fieldName: 'amount', type: 'currency' },
+    { label: 'CloseAt', fieldName: 'closeAt', type: 'date' },
+];
+
+
+export default class App extends LightningElement {
     clickedButtonLabel;
 
     handleClick(event) {
         this.clickedButtonLabel = event.target.label;
+        console.log(this.clickedButtonLabel);
+    }
+
+    data = [];
+    columns = columns;
+
+    // eslint-disable-next-line @lwc/lwc/no-async-await
+    async connectedCallback() {
+        const data = await fetchDataHelper({ amountOfRecords: 100 });
+        this.data = data;
     }
 }
